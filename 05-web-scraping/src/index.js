2c0bbe0fbf43e3d2167214c4a512049ff09f3abd
Vincent <vincent.tarrit29@gmail.com>
2022-04-29 09:18:15 +0200

    ok

--- 05-web-scraping/src/index.js
+++ 05-web-scraping/src/index.js
@@ -1,1 +1,11 @@
 import * as puppeteer from "puppeteer";
+
+
+/* (async() => {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.setViewport({ width: 1280, height: 800 })
+    await page.goto('https://fr.wikipedia.org/wiki/Canton_(Suisse)#Donn%C3%A9es_cantonales')
+    await page.screenshot({ path: 'example.png' })
+    await browser.close()
+})() */
